0970874ea519caa1ff6380d574c6d35b784c95a9
fltdeep007 <deepanshuj288@gmail.com>
2025-04-12 02:38:25 +0530

    Updated error

--- app.py
+++ app.py
@@ -836,5 +836,6 @@ def job_status(job_id):
 if __name__ == '__main__':
     print("="*80)
     print("Starting Flask Application")
+    print("Server running on: http://127.0.0.1:5000")
     print("="*80)
-    app.run( host='127.0.0.1', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
